dfa7f575953cb696d19a857304bdfe9d4d54b291
anirvan90 <anirvan.awatramani@gmail.com>
2017-07-04 18:15:37 -0700

    first commit

---
+++ src/lib/main.go
@@ -0,0 +1,81 @@
+package lib
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+const port = "8080"
+
+//RunHost takes an ip as argument
+//and listens for connections on that ip
+func RunHost(ip string) {
+
+	ipAndPort := ip + ":" + port
+	listener, listenErr := net.Listen("tcp", ipAndPort)
+	if listenErr != nil {
+		log.Fatal("Error: ", listenErr)
+	}
+	fmt.Println("Listening on", ipAndPort)
+
+	conn, acceptErr := listener.Accept()
+	if acceptErr != nil {
+		log.Fatal("Error: ", acceptErr)
+	}
+	fmt.Println("New Connection Accepted")
+	for {
+		handleHost(conn)
+	}
+}
+
+func handleHost(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	message, readErr := reader.ReadString('\n')
+	if readErr != nil {
+		log.Fatal("Error: ", readErr)
+	}
+	fmt.Println("Message Received: ", message)
+
+	fmt.Print("Send Message: ")
+	replyReader := bufio.NewReader(os.Stdin)
+	replyMessage, replyErr := replyReader.ReadString('\n')
+
+	if replyErr != nil {
+		log.Fatal("Error: ", replyErr)
+	}
+	fmt.Fprint(conn, replyMessage)
+}
+
+//RunGuest takes destination ip as argument
+//and connects to that ip
+func RunGuest(ip string) {
+	ipAndPort := ip + ":" + port
+	conn, dialErr := net.Dial("tcp", ipAndPort)
+	if dialErr != nil {
+		log.Fatal("Error: ", dialErr)
+	}
+	for {
+		handleGuest(conn)
+	}
+}
+
+func handleGuest(conn net.Conn) {
+	fmt.Print("Send Message: ")
+
+	reader := bufio.NewReader(os.Stdin)
+	message, readErr := reader.ReadString('\n')
+	if readErr != nil {
+		log.Fatal("Error: ", readErr)
+	}
+	fmt.Fprint(conn, message)
+
+	replyReader := bufio.NewReader(conn)
+	replyMessage, replyErr := replyReader.ReadString('\n')
+	if replyErr != nil {
+		log.Fatal("Error ", replyErr)
+	}
+	fmt.Println("Message Received: ", replyMessage)
+}
